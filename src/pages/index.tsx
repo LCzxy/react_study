395120ec99c376ff241744dec2205102b0567ce1
刘成 <admin@liuchengdeMacBook-Pro.local>
2021-09-23 17:14:24 +0800

    样式

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import styles from './index.less';
 
 export default function IndexPage() {
   return (
-    <div>
+    <div className="u-mgt10">
       <h1 className={styles.title}>Page index</h1>
     </div>
   );
